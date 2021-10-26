8f7944e2fd498010406808a3bc8ad19c8460f269
Maxim <maks.chugaev@yandex.ru>
2021-10-26 19:34:41 +0500

    new

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+
+<vue-form/>
+
+</template>
+
+<script>
+import VueForm from "./components/VueForm";
+
+export default {
+components: {VueForm}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/VueForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <form class="card">
+            <h1>Регистрация</h1>
+            <div class="form-card_reg">
+                <h2>Уже есть аккаунт?<a href="#">войти</a> </h2>
+
+            </div>
+            <div class="from-label">
+                <label for="name">Имя</label>
+                <input type="text" id="name" placeholder="Введите Ваше имя">
+            </div>
+            <div class="from-label">
+                <label for="email">email</label>
+                <input type="email" id="email" placeholder="Введите ваш email">
+            </div>
+            <div class="from-label">
+                <label for="language">Язык</label>
+                <select id="language">
+                    <option value="rus">Русский</option>
+                    <option value="eng">Английский</option>
+                    <option value="China">Китайский</option>
+                    <option value="spanish">Испанский</option>
+                </select>
+            </div>
+            <h2 class="rule">Принимаю <a href="#">условия</a> использования</h2>
+            <button class="btn" @click="">Зарегистрироваться</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
